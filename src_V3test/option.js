b61915f32b659c890ffe08f6cbcfd6158fafa5e3
yoshiteru <customer@nifty.com>
2021-10-23 19:24:31 +0900

    debug

--- src_V3test/option.js
+++ src_V3test/option.js
@@ -13,12 +13,14 @@
 
 document.getElementById('id_save').onclick = function() {
     chrome.storage.local.set({'prefix': document.getElementById('id_prefix').value},function(){});
+    console.log("prefix : ", document.getElementById('id_prefix').value);
+
     let positions = document.getElementsByName("name_position");
     for(let i = 0; i < positions.length; i++){
         if(positions[i].checked) {
             console.log("Radio button checked : ", positions[i].value);
             chrome.storage.local.set({'position': positions[i].value},function(){});
-            window.close()
+            // window.close()
         }
     }
 }
